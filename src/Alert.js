5362ac9af6f7dbd3b326527c3c4ff85b0a28cc25
= <=>
2022-12-26 13:12:39 +0700

    install babel-plugin-mmodule-resolver

---
+++ src/Alert.js
@@ -0,0 +1,7 @@
+function Alert() {
+    return (
+        alert("Click me!")
+    )
+}
+
+export default Alert
---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import Alert from "~/Alert"
+
+function Button() {
+    return (
+        <button onClick={Alert}>Click me!</button>
+    )
+}
+
+export default Button
