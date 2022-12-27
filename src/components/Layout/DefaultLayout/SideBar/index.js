ff594cf3048f071a426cbe1171f9b82b95684514
= <=>
2022-12-27 20:17:11 +0700

    Dựng khung DefaultLayout

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './SideBar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function SideBar() {
-    return <div>SideBar</div>;
+    return (
+        <aside className={cx('wrapper')}>
+            <div className="content">SideBar</div>
+        </aside>
+    );
 }
 
 export default SideBar;
